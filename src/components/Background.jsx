ba9ca19421a0b4118e96bf5a88899cd665bd0b42
HarshCh0uhan <harshcoding2004@gmail.com>
2026-01-08 10:38:26 +0530

    feat: add TV effect and static noise to Background component

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -7,6 +7,17 @@ import Instructions from './Instructions';
 const Background = () => {
   const randomeIndex = Math.floor(Math.random() * gifId.length); 
   const [currentGifIndex, setCurrentGifIndex] = useState(gifId[randomeIndex]);
+  const [isTvOn, setIsTvOn] = useState(false);
+  const [showContent, setShowContent] = useState(false);
+
+  useEffect(() => {
+    // Delay to show black screen first
+    setTimeout(() => setIsTvOn(true), 100);
+  }, []);
+
+  const handleStartClick = () => {
+    setShowContent(true);
+  };
 
   const gifUrl = gifURL + currentGifIndex + gifExtension;
 
@@ -27,7 +38,13 @@ const Background = () => {
   }, []);
 
   return (
-    <div onDoubleClick={handleChangeGif} className='relative h-screen w-screen overflow-hidden bg-black'>
+    <div onDoubleClick={handleChangeGif} className={`relative h-screen w-screen overflow-hidden bg-black ${isTvOn ? 'tv-screen' : ''}`}>
+      {/* TV Static Noise*/}
+      <div className='tv-static'></div> 
+
+      {/* Scanlines overlay */}
+      {isTvOn && <div className='scanlines'></div>}
+
       {/* Background GIF */}
       <img src={gifUrl} alt="gif" className='object-cover w-full h-full absolute inset-0' />
 
@@ -36,14 +53,15 @@ const Background = () => {
 
       {/* Terminal */}
       <div className="relative z-20 h-full flex flex-col md:flex-row justify-center
-       items-stretch gap-3 md:gap-[1%] p-2 md:p-[1%] mt-2 md:mt-[2%]">
-        <Terminal title={"--terminal"} window={false} changeFlex={"flex-3"} >
+      items-stretch gap-3 md:gap-[1%] p-2 md:p-[1%] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Terminal title={"--terminal"} window={false} changeFlex={"flex-3"}>
           <Intro />
         </Terminal>
         <Terminal title={"--help"} window={true} changeFlex={"flex-1"}>
           <Instructions />
         </Terminal>
       </div>
+
 
     </div>
   )
